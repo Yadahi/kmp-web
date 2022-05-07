4c4d9d8bd6254ee2cd676c065b5d17aedcfd03f1
Viliam <vatvil@gmail.com>
2022-05-07 23:01:22 +0200

    [FEATURE] style hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { useNavigate } from "react-router-dom";
 import heroImg from "../images/hero-section-img.png";
+import heroSectionImg from "../images/hero-section-boys-wave.png";
 
 function HeroSection() {
   let navigate = useNavigate();
@@ -30,48 +31,12 @@ function HeroSection() {
             </button>
           </div>
           <div className="hero-section__right">
-            {/* <img scr={heroImg} alt="hero" /> */}
-            <img src={heroImg} alt="hero" />
+            {/* <img src={heroImg} alt="hero" /> */}
+            <img src={heroSectionImg} alt="hero" />
           </div>
         </div>
       </div>
     </div>
-    // <div className="hero-container">
-    //   <div className="hero-container__wrapper">
-    //     <div className="hero-container__title">
-    //       <h1 className="h1">
-    //         KEĎ MOŽEM <span>POMOŽEM</span>
-    //       </h1>
-    //     </div>
-    //     <div className="hero-container__description">
-    //       <p className="text--red">
-    //         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-    //         eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-    //         ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    //         tempor incididunt ut labore et dolore magna aliqua.
-    //       </p>
-    //     </div>
-    //     <div className="hero-btns">
-    //       <Button
-    //         className="btns"
-    //         buttonStyle="btn--primary"
-    //         buttonSize="btn--medium"
-    //       >
-    //         GET STARTED
-    //       </Button>
-    //       <Button
-    //         className="btns"
-    //         buttonStyle="btn--primary"
-    //         buttonSize="btn--medium"
-    //       >
-    //         WATCH TRAILER
-    //       </Button>
-    //     </div>
-    //     <div className="hero-container__image">
-    //       <img src={heroImg} alt="kids" />
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
 
