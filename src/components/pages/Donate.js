3049a2b2eaa012fb29b83cc2fe4705ba37ef79a5
Viliam <vatvil@gmail.com>
2022-05-07 16:42:25 +0200

    [FEATURE] style donate page

--- src/components/pages/Donate.js
+++ src/components/pages/Donate.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import BreakerElement from "../objects/BreakerIcon";
 
@@ -6,12 +7,68 @@ function Donate() {
   return (
     <>
       <div className="layout">
-        <h1 className="h1 page-header bg-yellow font-blue">Podpor nas</h1>
-        <div className="layout__inner"></div>
-
-        <BreakerElement />
-
-        <BreakerElement />
+        <h1 className="h1 page-header page-header--bg-color--red text--color--white">
+          Podpor nas
+        </h1>
+        <div className="layout__inner">
+          <h2 className="text--center text--color--tertiary">
+            Ako nás môžete podporiť ?
+          </h2>
+          <div className="layout__section donate">
+            <div className="donate__left">
+              <h3>
+                Zapojte sa do našich{" "}
+                <strong>
+                  <Link to="/projekty">akcií</Link>
+                </strong>
+              </h3>
+              <p>&nbsp;</p>
+              <h3>
+                Zúčastnite sa s vašou firmou / zamestnávateľom nášho{" "}
+                <a
+                  href="https://www.facebook.com/events/2165929666972224/"
+                  target="_blank"
+                >
+                  futsalového turnaja
+                </a>
+              </h3>
+            </div>
+            <div className="donate__right">
+              <img src="../../images/img-donate.png " alt="" />
+            </div>
+          </div>
+          <BreakerElement />
+          <div className="layout__section donate">
+            <div className="donate__left">
+              <img
+                className="donate__img"
+                src="../../images/img-donate-second.png "
+                alt=""
+              />
+            </div>
+            <div className="donate__right">
+              <h3>
+                Darujte priamo na náš{" "}
+                <a
+                  href="https://ib.fio.sk/ib/transparent?a=2701658592"
+                  target="_blank"
+                >
+                  transparentný účet
+                </a>
+              </h3>
+              <p>
+                <strong>Číslo účtu:</strong>
+              </p>
+              <h4 className="text--color--red">
+                IBAN: SK19 8330 0000 0027 0165 8592
+              </h4>
+              <p>&nbsp;</p>
+              <h3>
+                Venujte nám <span className="text--color--red">2%</span> z dane
+              </h3>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
