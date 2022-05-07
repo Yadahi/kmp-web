6db1b00d282e53b7b728c6887bfc1f383c26296a
Viliam <vatvil@gmail.com>
2022-05-07 14:04:56 +0200

    [FEATURE] create contact us page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,12 +6,51 @@ function Contact() {
   return (
     <>
       <div className="layout">
-        <h1 className="h1 page-header bg-yellow font-blue">Kontakt</h1>
-        <div className="layout__inner"></div>
-
-        <BreakerElement />
-
-        <BreakerElement />
+        <h1 className="h1 page-header page-header--bg-color--secondary text--color--tertiary">
+          Kontakt
+        </h1>
+        <div className="layout__inner">
+          <div className="layout__section contact">
+            <div className="contact_address">
+              <p>Keď môžem pomôžem</p>
+              <p>Moldavská 9</p>
+              <p>040 11 Košice</p>
+              <p>IČO: 51278561</p>
+              <p>DIČ: 2120949556</p>
+            </div>
+            <div className="contact__media">
+              <div className="footer__icons">
+                <div className="social-icon">
+                  <a
+                    className="social-icon__link facebook"
+                    href="https://www.facebook.com/ozkedmozempomozem/"
+                    target="_blank"
+                    aria-label="Facebook"
+                  >
+                    <i className="fab fa-facebook-f fa-lg" />
+                  </a>
+                  <a
+                    className="social-icon__link youtube"
+                    href="https://www.youtube.com/channel/UCep8_UMzidsC4q8tG2Jk4nw"
+                    target="_blank"
+                    aria-label="Youtube"
+                  >
+                    <i className="fab fa-youtube fa-lg" />
+                  </a>
+                  <a
+                    className="social-icon__link twitter"
+                    href="https://www.twitter.com/KPomozem"
+                    target="_blank"
+                    aria-label="Twitter"
+                  >
+                    <i className="fab fa-twitter fa-lg" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <BreakerElement />
+        </div>
       </div>
     </>
   );
