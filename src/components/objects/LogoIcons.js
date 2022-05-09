72ee0a8401cb21babd9f3d162dc670400abcc60c
Viliam <vatvil@gmail.com>
2022-05-09 11:46:01 +0200

    [REFACTOR] add logic to logo component

--- src/components/objects/LogoIcons.js
+++ src/components/objects/LogoIcons.js
@@ -3,17 +3,17 @@ import { ReactComponent as LogoSvg } from "../../images/icons/logo.svg";
 
 import React from "react";
 
-function LogoIcon({ color = "gl-white" }) {
+function LogoIcon({ color = "gl-white", size = "" }) {
   return (
-    <div className={`logo-icon--color--${color}`}>
+    <div className={`logo-icon logo-icon--color--${color} ${size}`}>
       <LogoSvg />
     </div>
   );
 }
 
-function TextIcon({ color = "gl-white" }) {
+function TextIcon({ color = "gl-white", size = "", padding = "" }) {
   return (
-    <div className={`logo-icon--color--${color}`}>
+    <div className={`logo-icon logo-icon--color--${color} ${size} ${padding}`}>
       <TextSvg />
     </div>
   );
