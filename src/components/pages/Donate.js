d90fcd001d18136adc31678d5edab47ce3c53956
Viliam <vatvil@gmail.com>
2022-05-09 11:45:27 +0200

    [FEATURE] add text and style donate

--- src/components/pages/Donate.js
+++ src/components/pages/Donate.js
@@ -7,15 +7,41 @@ function Donate() {
   return (
     <>
       <div className="layout">
-        <h1 className="h1 page-header page-header--bg-color--gl-red text--color--gl-white">
-          Podpor nás
+        <h1 className="h1 page-header page-header--bg-color--secondary text--color--gl-red">
+          Keď môžeš podporíš
         </h1>
         <div className="layout__inner">
-          <h2 className="text--center text--color--tertiary">
+          <h3 className="text--center text--color--tertiary">
             Ako nás môžete podporiť ?
-          </h2>
+          </h3>
+          <p className="donate__annotation">
+            Ak máš nejaký ten finančný prebytok a hľadáš, kde by si ho rozumne
+            daroval, ponúkame ti možnosť. Dokonca viacero.
+          </p>
           <div className="layout__donate donate">
             <div className="donate__left">
+              <p>
+                Môžeš nám poslať jednorázovú platbu priamo na náš{" "}
+                <a
+                  href="https://ib.fio.sk/ib/transparent?a=2701658592"
+                  target="_blank"
+                >
+                  transparentný účet
+                </a>
+              </p>
+              <p>&nbsp;</p>
+              <p>
+                <strong>Číslo účtu:</strong>
+              </p>
+              <h4 className="text--color--gl-red">
+                IBAN: SK19 8330 0000 0027 0165 8592
+              </h4>
+              <p>&nbsp;</p>
+              <p>
+                Venujte nám <span className="text--color--gl-red">2%</span> z
+                dane.
+              </p>
+              <p>&nbsp;</p>
               <p>
                 Zapojte sa do našich <Link to="/projekty"> akcií</Link> alebo sa
                 zúčastnite s vašou firmou / zamestnávateľom nášho{" "}
@@ -23,7 +49,7 @@ function Donate() {
                   href="https://www.facebook.com/events/2165929666972224/"
                   target="_blank"
                 >
-                  futsalového turnaja
+                  futsalového turnaja.
                 </a>
               </p>
             </div>
@@ -34,74 +60,6 @@ function Donate() {
                 class="donate__img"
               />
             </div>
-          </div>
-        </div>
-        <div className="layout background--bg-tints--primary-200">
-          <div className="layout__inner">
-            <div className="layout__donate donate">
-              <div className="donate__left donate__img">
-                <img
-                  className="donate__img"
-                  src="../../images/img-donate-second.png "
-                  alt=""
-                />
-              </div>
-              <div className="donate__right">
-                <p>
-                  Darujte priamo na náš{" "}
-                  <a
-                    href="https://ib.fio.sk/ib/transparent?a=2701658592"
-                    target="_blank"
-                  >
-                    transparentný účet
-                  </a>
-                </p>
-                <p>&nbsp;</p>
-                <p>
-                  <strong>Číslo účtu:</strong>
-                </p>
-                <h4 className="text--color--gl-red">
-                  IBAN: SK19 8330 0000 0027 0165 8592
-                </h4>
-                <p>&nbsp;</p>
-                <p>
-                  Venujte nám <span className="text--color--gl-red">2%</span> z
-                  dane
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="layout__inner">
-          <h2 className="text--center text--color--tertiary">
-            Ďakujeme partnerom &#10084;
-          </h2>
-          <div className="layout__partners partners margin-top-1">
-            <img
-              src="../../images/partnery-1.png"
-              alt=""
-              className="partners__img img-1"
-            />
-            <img
-              src="../../images/partnery-2.png"
-              alt=""
-              className="partners__img img-2"
-            />
-            <img
-              src="../../images/partnery-3.png"
-              alt=""
-              className="partners__img img-3"
-            />
-            <img
-              src="../../images/partnery-4.jpg"
-              alt=""
-              className="partners__img img-4"
-            />
-            <img
-              src="../../images/partnery-5.jpg"
-              alt=""
-              className="partners__img img-5"
-            />
           </div>
         </div>
       </div>
